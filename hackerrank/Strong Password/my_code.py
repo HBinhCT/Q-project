576f5768021333c65e8a79a3c0967aca8b1b73a5
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-03-16 14:47:53 +0700

    [ADD] Strong Password

---
+++ hackerrank/Strong Password/my_code.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the minimumNumber function below.
+def minimumNumber(n, password):
+    # Return the minimum number of characters to make the password strong
+    count = 0
+    cases = [r'[a-z]', r'[A-Z]', r'[\d]', r'[!@#$%^&*()\-+]']
+    for case in cases:
+        if not re.search(case, password):
+            count += 1
+    return max(count, 6 - n)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    password = input()
+
+    answer = minimumNumber(n, password)
+
+    fptr.write(str(answer) + '\n')
+
+    fptr.close()
