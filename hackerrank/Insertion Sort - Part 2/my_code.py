649de2ac025d9f15a713a807a9859725bc1fd6f8
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-03-16 14:50:33 +0700

    [ADD] Insertion Sort - Part 2

---
+++ hackerrank/Insertion Sort - Part 2/my_code.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the insertionSort2 function below.
+def insertionSort2(n, arr):
+    amin = -10 ** 4
+    arr = [2 * amin] + arr
+    for j in range(2, n + 1):
+        i, a = j, arr[j]
+        while a < arr[i - 1]:
+            arr[i] = arr[i - 1]
+            i -= 1
+        arr[i] = a
+        print(*arr[1:])
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort2(n, arr)
