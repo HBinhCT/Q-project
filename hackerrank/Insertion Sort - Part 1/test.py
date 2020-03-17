c857ce87bc8ac9b09405cdabefe20e569f7085a4
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-03-17 09:00:17 +0700

    [ADD] Insertion Sort - Part 1

---
+++ hackerrank/Insertion Sort - Part 1/test.py
@@ -0,0 +1,16 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+import my_code
+
+
+class TestCaesarCipher(unittest.TestCase):
+    def test_case_0(self):
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            my_code.insertionSort1(5, [2, 4, 6, 8, 3])
+        self.assertEqual(text_trap.getvalue(), "2 4 6 8 8\n2 4 6 6 8\n2 4 4 6 8\n2 3 4 6 8\n")
+
+
+if __name__ == '__main__':
+    unittest.main()
