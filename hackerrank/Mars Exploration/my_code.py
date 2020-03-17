5893c1bde9e7fe784139d2da451f238b84223f06
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-03-17 13:26:08 +0700

    [ADD] Mars Exploration

---
+++ hackerrank/Mars Exploration/my_code.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the marsExploration function below.
+def marsExploration(s):
+    return sum(s[i] != 'SOS'[i % 3] for i in range(len(s)))
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = marsExploration(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
---
+++ hackerrank/Mars Exploration/test.py
@@ -0,0 +1,17 @@
+import unittest
+import my_code
+
+
+class TestCaesarCipher(unittest.TestCase):
+    def test_case_0(self):
+        self.assertEqual(my_code.marsExploration('SOSSPSSQSSOR'), 3)
+
+    def test_case_1(self):
+        self.assertEqual(my_code.marsExploration('SOSSOT'), 1)
+
+    def test_case_2(self):
+        self.assertEqual(my_code.marsExploration('SOSSOSSOS'), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
