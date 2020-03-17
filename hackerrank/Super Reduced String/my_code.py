01b6502ba4720803ba7ea79d32cf23ed89bdee02
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-03-17 09:11:55 +0700

    [ADD] Super Reduced String

---
+++ hackerrank/Super Reduced String/my_code.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the superReducedString function below.
+def superReducedString(s):
+    res = []  # stack
+    for c in s:
+        if res and res[-1] == c:  # peek what's on top
+            res.pop()
+        else:
+            res.append(c)
+    res = ''.join(res)
+    return res or 'Empty String'
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = superReducedString(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
---
+++ hackerrank/Super Reduced String/test.py
@@ -0,0 +1,17 @@
+import unittest
+import my_code
+
+
+class TestCaesarCipher(unittest.TestCase):
+    def test_case_0(self):
+        self.assertEqual(my_code.superReducedString('aaabccddd'), 'abd')
+
+    def test_case_1(self):
+        self.assertEqual(my_code.superReducedString('aa'), 'Empty String')
+
+    def test_case_2(self):
+        self.assertEqual(my_code.superReducedString('baab'), 'Empty String')
+
+
+if __name__ == '__main__':
+    unittest.main()
