a4b3227f2378f541aeff6d6bcac4ba72d3f1cecd
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-03-18 13:56:53 +0700

    [ADD] Running Time of Algorithms

---
+++ hackerrank/Running Time of Algorithms/my_code.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the runningTime function below.
+def runningTime(arr):
+    total = 0
+    for i in range(1, len(arr)):
+        j = i
+        key = arr[i]
+        del arr[i]
+        while j and key < arr[j - 1]:
+            j -= 1
+        arr.insert(j, key)
+        total += i - j
+    return total
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = runningTime(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
---
+++ hackerrank/Running Time of Algorithms/test.py
@@ -0,0 +1,14 @@
+import unittest
+import my_code
+
+
+class TestCaesarCipher(unittest.TestCase):
+    def test_case_0(self):
+        self.assertEqual(my_code.runningTime([2, 1, 3, 1, 2]), 4)
+
+    def test_case_1(self):
+        self.assertEqual(my_code.runningTime([1, 1, 2, 2, 3, 3, 5, 5, 7, 7, 9, 9]), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
