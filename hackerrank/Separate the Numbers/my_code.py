564c50ea9dc64a51a63f58277f23cfb104727a20
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-03-23 13:45:53 +0700

    [ADD] Separate the Numbers

---
+++ hackerrank/Separate the Numbers/my_code.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the separateNumbers function below.
+def separateNumbers(s):
+    n = len(s)
+    i = 1
+    while i <= n // 2:
+        j = i
+        a = int(s[:i])
+        flag = 1
+        while j < n:
+            b = str(a + 1)
+            if s[j:j + len(b)] != b:
+                flag = 0
+                break
+            j += len(b)
+            a = int(b)
+        if flag:
+            print('YES ' + s[:i])
+            return
+        i += 1
+    print('NO')
+    return
+
+
+if __name__ == '__main__':
+    q = int(input())
+
+    for q_itr in range(q):
+        s = input()
+
+        separateNumbers(s)
---
+++ hackerrank/Separate the Numbers/test.py
@@ -0,0 +1,28 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+import my_code
+
+
+class TestCaesarCipher(unittest.TestCase):
+    def test_case_0(self):
+        input_stdin = ('1234', '91011', '99100', '101103', '010203', '13', '1')
+        output_stdout = ('YES 1', 'YES 9', 'YES 99', 'NO', 'NO', 'NO', 'NO')
+        for i in range(len(input_stdin)):
+            text_trap = io.StringIO()
+            with redirect_stdout(text_trap):
+                my_code.separateNumbers(input_stdin[i])
+            self.assertEqual(text_trap.getvalue(), output_stdout[i] + "\n")
+
+    def test_case_1(self):
+        input_stdin = ('99910001001', '7891011', '9899100', '999100010001')
+        output_stdout = ('YES 999', 'YES 7', 'YES 98', 'NO')
+        for i in range(len(input_stdin)):
+            text_trap = io.StringIO()
+            with redirect_stdout(text_trap):
+                my_code.separateNumbers(input_stdin[i])
+            self.assertEqual(text_trap.getvalue(), output_stdout[i] + "\n")
+
+
+if __name__ == '__main__':
+    unittest.main()
