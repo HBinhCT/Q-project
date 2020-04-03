12f1ab7591690ba911c5d383edbb6fa9c33be159
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-04-03 08:27:18 +0700

    [ADD] Staircase

---
+++ hackerrank/Staircase/README.md
@@ -0,0 +1,3 @@
+https://www.hackerrank.com/challenges/staircase/problem
+
+[Explain](https://www.hackerrank.com/challenges/staircase/forum/comments/610431)
---
+++ hackerrank/Staircase/test.py
@@ -0,0 +1,22 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+import my_code
+
+
+class TestQ(unittest.TestCase):
+    def test_case_0(self):
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            my_code.staircase(6)
+        self.assertEqual(text_trap.getvalue(), "     #\n    ##\n   ###\n  ####\n #####\n######\n")
+        #      #
+        #     ##
+        #    ###
+        #   ####
+        #  #####
+        # ######
+
+
+if __name__ == '__main__':
+    unittest.main()
