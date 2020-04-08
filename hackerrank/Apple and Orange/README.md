1e781c4befcf0c1be3aeef7fa45a38c4082f3645
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-04-08 07:52:53 +0700

    [ADD] Apple and Orange

---
+++ hackerrank/Apple and Orange/README.md
@@ -0,0 +1,3 @@
+https://www.hackerrank.com/challenges/apple-and-orange/problem
+
+[Explain](https://www.hackerrank.com/challenges/apple-and-orange/forum/comments/210896)
---
+++ hackerrank/Apple and Orange/my_code.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the countApplesAndOranges function below.
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    print(sum(s <= a + d <= t for d in apples))
+    print(sum(s <= b + d <= t for d in oranges))
+
+
+if __name__ == '__main__':
+    st = input().split()
+
+    s = int(st[0])
+
+    t = int(st[1])
+
+    ab = input().split()
+
+    a = int(ab[0])
+
+    b = int(ab[1])
+
+    mn = input().split()
+
+    m = int(mn[0])
+
+    n = int(mn[1])
+
+    apples = list(map(int, input().rstrip().split()))
+
+    oranges = list(map(int, input().rstrip().split()))
+
+    countApplesAndOranges(s, t, a, b, apples, oranges)
