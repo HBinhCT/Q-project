91e03b5bbe77c5e0894b76e56dd8cbe97eb3b8eb
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-04-10 08:30:13 +0700

    [ADD] Bon Appétit

---
+++ hackerrank/Bon Appétit/README.md
@@ -0,0 +1,3 @@
+https://www.hackerrank.com/challenges/bon-appetit/problem
+
+[Explain](https://www.hackerrank.com/challenges/bon-appetit/forum/comments/229499)
---
+++ hackerrank/Bon Appétit/my_code.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the bonAppetit function below.
+def bonAppetit(bill, k, b):
+    print(b - (sum(bill) - bill[k]) // 2 or 'Bon Appetit')
+
+
+if __name__ == '__main__':
+    nk = input().rstrip().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    bonAppetit(bill, k, b)
