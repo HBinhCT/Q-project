646ea34e181a363b12ed71b14d580458767ae403
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-04-10 14:10:06 +0700

    [ADD] Sock Merchant

---
+++ hackerrank/Sock Merchant/README.md
@@ -0,0 +1,3 @@
+https://www.hackerrank.com/challenges/sock-merchant/problem
+
+[Explain](https://www.hackerrank.com/challenges/sock-merchant/forum/comments/195689)
---
+++ hackerrank/Sock Merchant/test.py
@@ -0,0 +1,14 @@
+import unittest
+import my_code
+
+
+class TestQ(unittest.TestCase):
+    def test_case_0(self):
+        self.assertEqual(my_code.sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20]), 3)
+
+    def test_case_1(self):
+        self.assertEqual(my_code.sockMerchant(10, [1, 1, 3, 1, 2, 1, 3, 3, 3, 3]), 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
