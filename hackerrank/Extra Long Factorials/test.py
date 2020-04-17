178c4d7d61136577b17f3e1c929261f03c91c3b3
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-04-17 09:19:33 +0700

    [ADD] Extra Long Factorials

---
+++ hackerrank/Extra Long Factorials/test.py
@@ -0,0 +1,22 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+import my_code
+
+
+class TestQ(unittest.TestCase):
+    def test_case_0(self):
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            my_code.extraLongFactorials(25)
+        self.assertEqual(text_trap.getvalue(), "15511210043330985984000000\n")
+
+    def test_case_1(self):
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            my_code.extraLongFactorials(45)
+        self.assertEqual(text_trap.getvalue(), "119622220865480194561963161495657715064383733760000000000\n")
+
+
+if __name__ == '__main__':
+    unittest.main()
