12791c74e4c8e6719b4e1087911bc3733cb071e3
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-04-20 10:20:04 +0700

    [ADD] Cut the sticks

---
+++ hackerrank/Cut the sticks/README.md
@@ -0,0 +1,3 @@
+https://www.hackerrank.com/challenges/cut-the-sticks/problem
+
+[Explain](https://www.hackerrank.com/challenges/cut-the-sticks/forum/comments/709163)
---
+++ hackerrank/Cut the sticks/my_code.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the cutTheSticks function below.
+def cutTheSticks(arr):
+    from collections import Counter
+    ln = len(arr)
+    c = Counter(arr)
+    for k in sorted(c):
+        yield ln
+        ln -= c[k]
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = cutTheSticks(arr)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
