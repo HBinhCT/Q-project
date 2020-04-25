e93596c628612a85bf107faecc896e3582d8d85b
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-04-25 22:05:50 +0700

    [ADD] Python If-Else

---
+++ hackerrank/Python/Python If-Else/my_code.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    check = {True: 'Not Weird', False: 'Weird'}
+    print(check[n % 2 == 0 and n not in range(6, 21)])
