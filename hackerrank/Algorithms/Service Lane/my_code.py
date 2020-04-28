3b599ce1e2317d26622b12984a35e5a8fa1845de
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-04-28 11:42:24 +0700

    [ADD] Service Lane

---
+++ hackerrank/Algorithms/Service Lane/my_code.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the serviceLane function below.
+def serviceLane(width, cases):
+    return [min(width[s:e + 1]) for s, e in cases]
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nt = input().split()
+
+    n = int(nt[0])
+
+    t = int(nt[1])
+
+    width = list(map(int, input().rstrip().split()))
+
+    cases = []
+
+    for _ in range(t):
+        cases.append(list(map(int, input().rstrip().split())))
+
+    result = serviceLane(width, cases)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
---
+++ hackerrank/Algorithms/Service Lane/test.py
@@ -0,0 +1,16 @@
+import unittest
+import my_code
+
+
+class TestQ(unittest.TestCase):
+    def test_case_0(self):
+        self.assertEqual(my_code.serviceLane([2, 3, 1, 2, 3, 2, 3, 3], [[0, 3], [4, 6], [6, 7], [3, 5], [0, 7]]),
+                         [1, 2, 3, 2, 1])
+
+    def test_case_1(self):
+        self.assertEqual(my_code.serviceLane([1, 2, 2, 2, 1], [[2, 3], [1, 4], [2, 4], [2, 4], [2, 3]]),
+                         [2, 1, 1, 1, 2])
+
+
+if __name__ == '__main__':
+    unittest.main()
