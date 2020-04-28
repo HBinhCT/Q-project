07a6d8fb16f636ae8b4036447dede754992ed6d4
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-04-28 08:39:05 +0700

    [ADD] The Time in Words

---
+++ hackerrank/Algorithms/The Time in Words/my_code.py
@@ -0,0 +1,56 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the timeInWords function below.
+def timeInWords(h, m):
+    times = [
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine',
+        'ten',
+        'eleven',
+        'twelve',
+        'thirteen',
+        'fourteen',
+        'fifteen',
+        'sixteen',
+        'seventeen',
+        'eighteen',
+        'nineteen',
+        'twenty',
+    ]
+    for i in range(1, 10):
+        times.append(f'twenty {times[i - 1]}')
+    formats = ["{} o' clock", "quarter past {}", "half past {}", "quarter to {}"]
+    hour = times[h - 1] if (m < 31) else times[h]
+    if m % 15 == 0:
+        return formats[m // 15].format(hour)
+    if m < 30:
+        return f"{times[m - 1]} minute{'s' * (m != 1)} past {hour}"
+    return f"{times[60 - m - 1]} minute{'s' * (m != 59)} to {hour}"
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    h = int(input())
+
+    m = int(input())
+
+    result = timeInWords(h, m)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
