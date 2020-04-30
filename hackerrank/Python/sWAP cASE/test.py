f754c431a5db57b4744946f622e21b2563d1963f
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-04-30 14:13:43 +0700

    [ADD] sWAP cASE

---
+++ hackerrank/Python/sWAP cASE/test.py
@@ -0,0 +1,12 @@
+import unittest
+import solution
+
+
+class TestQ(unittest.TestCase):
+    def test_case_0(self):
+        self.assertEqual(solution.swap_case('HackerRank.com presents "Pythonist 2".'),
+                         'hACKERrANK.COM PRESENTS "pYTHONIST 2".')
+
+
+if __name__ == '__main__':
+    unittest.main()
