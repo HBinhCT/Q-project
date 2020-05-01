ddbfb837474953bc781717cfcfa5248833ae0110
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-05-01 13:46:13 +0700

    [ADD] Mutations

---
+++ hackerrank/Python/Mutations/test.py
@@ -0,0 +1,11 @@
+import unittest
+import solution
+
+
+class TestQ(unittest.TestCase):
+    def test_case_0(self):
+        self.assertEqual(solution.mutate_string('abracadabra', 5, 'k'), 'abrackdabra')
+
+
+if __name__ == '__main__':
+    unittest.main()
