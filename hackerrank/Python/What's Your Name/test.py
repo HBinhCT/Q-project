f2399d1816946a38494cca03a599d381a15718c7
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-05-01 13:42:56 +0700

    [ADD] What's Your Name?

---
+++ hackerrank/Python/What's Your Name/test.py
@@ -0,0 +1,16 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+import solution
+
+
+class TestQ(unittest.TestCase):
+    def test_case_0(self):
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            solution.print_full_name('Ross', 'Taylor')
+        self.assertEqual(text_trap.getvalue(), 'Hello Ross Taylor! You just delved into python.\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
