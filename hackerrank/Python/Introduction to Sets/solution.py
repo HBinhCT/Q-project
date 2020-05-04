9351b696307c243f2635064ca1d89477131c1fa3
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-05-04 13:19:42 +0700

    [ADD] Introduction to Sets

---
+++ hackerrank/Python/Introduction to Sets/solution.py
@@ -0,0 +1,11 @@
+def average(array):
+    # your code goes here
+    heights = set(array)
+    return sum(heights) / len(heights)
+
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
---
+++ hackerrank/Python/Introduction to Sets/test.py
@@ -0,0 +1,11 @@
+import unittest
+import solution
+
+
+class TestQ(unittest.TestCase):
+    def test_case_0(self):
+        self.assertEqual(solution.average([161, 182, 161, 154, 176, 170, 167, 171, 170, 174]), 169.375)
+
+
+if __name__ == '__main__':
+    unittest.main()
