59ffce42e9428c4f1a1bdc76e0adcab89a7ada02
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-05-04 13:56:06 +0700

    [ADD] Fair Rations

---
+++ hackerrank/Algorithms/Fair Rations/README.md
@@ -0,0 +1,4 @@
+https://www.hackerrank.com/challenges/fair-rations/problem
+
+Explain:
+- https://www.hackerrank.com/challenges/fair-rations/forum/comments/709322
---
+++ hackerrank/Algorithms/Fair Rations/solution.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the fairRations function below.
+def fairRations(B):
+    total = 0
+    for i in range(len(B) - 1):
+        if B[i] % 2:
+            total += 2
+            B[i + 1] += 1
+    return 'NO' if B[-1] % 2 else total
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    N = int(input())
+
+    B = list(map(int, input().rstrip().split()))
+
+    result = fairRations(B)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
