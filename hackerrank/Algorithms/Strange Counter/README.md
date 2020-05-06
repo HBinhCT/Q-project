e1bd56223ac101e4d88fc457cabbd32d1d8871b4
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-05-06 15:29:08 +0700

    [ADD] Strange Counter

---
+++ hackerrank/Algorithms/Strange Counter/README.md
@@ -0,0 +1,4 @@
+https://www.hackerrank.com/challenges/strange-code/problem
+
+Explain:
+- https://www.hackerrank.com/challenges/strange-code/forum/comments/583337
---
+++ hackerrank/Algorithms/Strange Counter/solution.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the strangeCounter function below.
+def strangeCounter(t):
+    return 6 * 2 ** int(math.log2((t + 2) / 3)) - (t + 2)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    result = strangeCounter(t)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
