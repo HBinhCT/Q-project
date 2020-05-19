4478c965a136a1a8fae2eb42e5bec0d0b3938c81
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-05-19 10:03:17 +0700

    [ADD] Company Logo

---
+++ hackerrank/Python/Company Logo/README.md
@@ -0,0 +1,4 @@
+https://www.hackerrank.com/challenges/most-commons/problem
+
+Explain:
+- https://www.hackerrank.com/challenges/most-commons/forum/comments/711310
---
+++ hackerrank/Python/Company Logo/solution.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def most_commons(logo):
+    from collections import Counter
+    count = Counter(logo)
+    return [f'{k} {v}' for v in sorted(set(count.values()), reverse=True) for k in
+            sorted(k for k in count if count[k] == v)][:3]
+
+
+if __name__ == '__main__':
+    s = input()
+    assert 3 < len(s) <= 10 ** 4
+    result = most_commons(s)
+    print(*result, sep='\n')
