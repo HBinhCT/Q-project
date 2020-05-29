b88f3f65e0b42929f49fb594748a5bee9494a1e6
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-05-29 16:57:21 +0700

    [ADD] Matrix Script

---
+++ hackerrank/Python/Matrix Script/solution.py
@@ -0,0 +1,24 @@
+# !/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+first_multiple_input = input().rstrip().split()
+
+n = int(first_multiple_input[0])
+
+m = int(first_multiple_input[1])
+
+matrix = []
+
+for _ in range(n):
+    matrix_item = input()
+    matrix.append(matrix_item)
+
+script = ''
+for row in zip(*matrix):
+    script += ''.join(row)
+print(re.sub(r'(?<=[A-Za-z0-9])([^A-Za-z0-9]+)(?=[A-Za-z0-9])', ' ', script))
