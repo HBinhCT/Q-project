87ef01d9f3dedb034e21a25d2364d06c1daf239b
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-06-10 10:08:51 +0700

    [ADD] Testing with Jest and rewire

---
+++ hackerrank/10 Days of Javascript/Day 0 - Data Types/test.js
@@ -0,0 +1,13 @@
+const rewire = require('rewire');
+
+const solution = rewire('./solution')
+const logMock = jest.fn();
+solution.__set__('console', {log: logMock});
+
+test('Test case 0', async () => {
+    solution.__get__('performOperation')('12', '4.32', 'is the best place to learn and practice coding!');
+    expect(logMock.mock.calls.length).toBe(3)
+    expect(logMock.mock.calls[0][0]).toBe(16);
+    expect(logMock.mock.calls[1][0]).toBe(8.32);
+    expect(logMock.mock.calls[2][0]).toBe('HackerRank is the best place to learn and practice coding!');
+})
---
+++ hackerrank/10 Days of Javascript/Day 0 - Hello, World!/test.js
@@ -0,0 +1,12 @@
+const rewire = require('rewire');
+
+const solution = rewire('./solution')
+const logMock = jest.fn();
+solution.__set__('console', {log: logMock});
+
+test('Test case 0', async () => {
+    solution.__get__('greeting')('Welcome to 10 Days of JavaScript!');
+    expect(logMock.mock.calls.length).toBe(2)
+    expect(logMock.mock.calls[0][0]).toBe('Hello, World!');
+    expect(logMock.mock.calls[1][0]).toBe('Welcome to 10 Days of JavaScript!');
+})
