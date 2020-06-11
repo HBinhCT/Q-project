954e11b034b4d4cb5a93f4c9f0117fefcfa121fd
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-06-11 11:47:59 +0700

    [ADD] Day 1: Let and Const

---
+++ hackerrank/10 Days of Javascript/Day 1 - Let and Const/test.js
@@ -0,0 +1,16 @@
+const rewire = require('rewire');
+
+const solution = rewire('./solution')
+const logMock = jest.fn();
+const errorMock = jest.fn();
+solution.__set__('console', {log: logMock, error: errorMock});
+
+test('Test case 0', async () => {
+    solution.__set__('readLine', jest.fn(() => 2.6));
+    solution.__get__('main')();
+    expect(logMock.mock.calls.length).toBe(2);
+    expect(logMock.mock.calls[0][0]).toBe(21.237166338267002);
+    expect(logMock.mock.calls[1][0]).toBe(16.336281798666924);
+    expect(errorMock.mock.calls.length).toBe(1);
+    expect(errorMock.mock.calls[0][0]).toBe("You correctly declared 'PI' as a constant.");
+})
