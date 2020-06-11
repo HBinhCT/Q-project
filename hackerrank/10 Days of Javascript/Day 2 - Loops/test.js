52d7462802cbb08ad98a945f10ed7364dc1a0fdc
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-06-11 13:40:03 +0700

    [ADD] Day 2: Loops

---
+++ hackerrank/10 Days of Javascript/Day 2 - Loops/test.js
@@ -0,0 +1,27 @@
+const rewire = require('rewire');
+
+const solution = rewire('./solution');
+const logMock = jest.fn();
+solution.__set__('console', {log: logMock});
+
+test('Test case 0', async () => {
+    solution.__get__('vowelsAndConsonants')('javascriptloops');
+    const result = logMock.mock.calls.map(value => value[0]).join('\n');
+    expect(result).toBe(
+        'a\n' +
+        'a\n' +
+        'i\n' +
+        'o\n' +
+        'o\n' +
+        'j\n' +
+        'v\n' +
+        's\n' +
+        'c\n' +
+        'r\n' +
+        'p\n' +
+        't\n' +
+        'l\n' +
+        'p\n' +
+        's'
+    );
+})
