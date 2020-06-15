7dcd9fc0981376b836d9ecad2981853bc64bc675
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-06-15 14:55:33 +0700

    [ADD] Day 0: Hello, World.

---
+++ hackerrank/30 Days of Code/Day 0 - Hello, World/test.py
@@ -0,0 +1,20 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+from unittest.mock import patch
+
+
+class TestQ(unittest.TestCase):
+    @patch('builtins.input', return_value='Welcome to 30 Days of Code!')
+    def test_case_0(self, input_values=None):
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            import solution
+        self.assertEqual(text_trap.getvalue(),
+                         'Hello, World.' + '\n' +
+                         'Welcome to 30 Days of Code!' + '\n'
+                         )
+
+
+if __name__ == '__main__':
+    unittest.main()
