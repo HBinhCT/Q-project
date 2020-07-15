7a83c2996fe664675115eca51824897b97415c26
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-07-15 10:50:08 +0700

    [ADD] Hexadecimal numbers

---
+++ hackerearth/Algorithms/Hexadecimal numbers/README.md
@@ -0,0 +1,1 @@
+https://www.hackerearth.com/practice/algorithms/searching/linear-search/practice-problems/algorithm/yet-another-easy-problem-1f3273a0/
---
+++ hackerearth/Algorithms/Hexadecimal numbers/solution.py
@@ -0,0 +1,28 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+import math
+
+t = int(input())
+lefts = []
+rights = []
+for _ in range(t):
+    l, r = map(int, input().strip().split())
+    lefts.append(l)
+    rights.append(r)
+counter = dict()
+count = 0
+for x in range(min(lefts) - 1, max(rights) + 1):
+    fx = sum(int(s, 16) for s in f'{x:02x}')
+    if math.gcd(x, fx) > 1:
+        count += 1
+    counter[x] = count
+for left, right in zip(lefts, rights):
+    print(counter[right] - counter[left - 1])
