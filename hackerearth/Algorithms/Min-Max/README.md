db9a6bc26ef730251d3c6e8c814483d8a1491a7e
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-07-17 10:43:59 +0700

    [ADD] Min-Max

---
+++ hackerearth/Algorithms/Min-Max/README.md
@@ -0,0 +1,1 @@
+https://www.hackerearth.com/practice/algorithms/searching/linear-search/practice-problems/algorithm/min-max-8/
---
+++ hackerearth/Algorithms/Min-Max/solution.py
@@ -0,0 +1,14 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+n = int(input())
+a = list(map(int, input().strip().split()))
+total = sum(a)
+print(total - max(a), total - min(a))
