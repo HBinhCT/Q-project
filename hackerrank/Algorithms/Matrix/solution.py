881e9934537238b4b14a242fa217ed53f7cc5251
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-07-28 14:06:55 +0700

    [ADD] Matrix

---
+++ hackerrank/Algorithms/Matrix/solution.py
@@ -0,0 +1,64 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the minTime function below.
+def minTime(roads, machines):
+    from collections import defaultdict
+
+    parent = {}
+    dp = defaultdict(int)  # dp[i] denotes whether or not component with root i had already had a machine
+    for machine in machines:
+        dp[machine] = 1
+
+    def find(node):
+        if parent.get(node, node) == node:
+            return node
+        x = find(parent[node])
+        parent[node] = x
+        return x
+
+    def union(i, j):
+        x, y = find(i), find(j)
+        if not dp[x] or not dp[y]:
+            if i != x:
+                x, y = y, x
+            parent[x] = y
+            dp[x] |= dp[y]
+            dp[y] |= dp[x]
+            return True
+        return False
+
+    return sum(time for i, j, time in sorted(roads, key=lambda i: -i[-1]) if not union(i, j))
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    roads = []
+
+    for _ in range(n - 1):
+        roads.append(list(map(int, input().rstrip().split())))
+
+    machines = []
+
+    for _ in range(k):
+        machines_item = int(input())
+        machines.append(machines_item)
+
+    result = minTime(roads, machines)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
---
+++ hackerrank/Algorithms/Matrix/test.py
@@ -0,0 +1,17 @@
+import unittest
+import solution
+
+
+class TestQ(unittest.TestCase):
+    def test_case_0(self):
+        self.assertEqual(solution.minTime([[2, 1, 8], [1, 0, 5], [2, 4, 5], [1, 3, 4]], [2, 4, 0]), 10)
+
+    def test_case_1(self):
+        self.assertEqual(solution.minTime([[0, 1, 4], [1, 2, 3], [1, 3, 7], [0, 4, 2]], [2, 3, 4]), 5)
+
+    def test_case_2(self):
+        self.assertEqual(solution.minTime([[0, 3, 3], [1, 4, 4], [1, 3, 4], [0, 2, 5]], [1, 3, 4]), 8)
+
+
+if __name__ == '__main__':
+    unittest.main()
