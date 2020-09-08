bded7e7b34b95410361b94c21f5185c5a0651af7
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-09-08 15:35:01 +0700

    [ADD] Triangular Ranges

---
+++ hackerearth/Algorithms/Triangular Ranges/README.md
@@ -0,0 +1,1 @@
+https://www.hackerearth.com/practice/algorithms/searching/binary-search/practice-problems/algorithm/triangular-ranges-2/
---
+++ hackerearth/Algorithms/Triangular Ranges/solution.py
@@ -0,0 +1,28 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+q = int(input())
+for _ in range(q):
+    left, right = map(int, input().strip().split())
+    ans = 0
+    left *= 2  # The triangle numbers are given by the following explicit formulas: n(n + 1)/2
+    right *= 2
+    a, b, c = 1, 2000000, 2000000  # √(10^12) = 1000000
+    while a < c:
+        x = a * (a + 1)
+        while a < c and x + (c - 1) * c > right:
+            c -= 1
+        b = min(b, c)
+        while a <= b and x + (b - 1) * b >= left:
+            b -= 1
+        if a < c:
+            ans += c - max(a, b)
+        a += 1
+    print(ans)
