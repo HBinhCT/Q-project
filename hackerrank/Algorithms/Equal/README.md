be2330b3d0d1ff74a1c1bd2c7d635e4285341c42
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-09-12 20:57:25 +0700

    [ADD] Equal

---
+++ hackerrank/Algorithms/Equal/README.md
@@ -0,0 +1,5 @@
+https://www.hackerrank.com/challenges/equal/problem
+
+Explain:
+- https://www.hackerrank.com/challenges/equal/forum/comments/575147
+- https://www.hackerrank.com/challenges/equal/forum/comments/663042
---
+++ hackerrank/Algorithms/Equal/solution.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the equal function below.
+def equal(arr):
+    low = min(arr)
+    res = float('inf')
+    for base in range(0, 3):
+        current_sum = 0
+        for i in range(len(arr)):
+            diff = arr[i] - low + base
+            current_sum += diff // 5 + diff % 5 // 2 + diff % 5 % 2  # // 1
+        res = min(current_sum, res)
+    return res
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = equal(arr)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
