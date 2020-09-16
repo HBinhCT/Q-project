2e7ef10ee9f344bdc1b58872d6901ac652b8ebb6
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-09-16 14:57:21 +0700

    [ADD] Xsquare And Number List

---
+++ hackerearth/Algorithms/Xsquare And Number List/solution.py
@@ -0,0 +1,33 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+import sys
+from bisect import bisect
+
+n, q = map(int, sys.stdin.readline().strip().split())
+elements = sorted(map(int, sys.stdin.readline().strip().split()))
+mod = 1000000007  # 10 ** 9 + 7
+p = [1] * (n + 1)
+for i in range(1, n + 1):
+    p[i] = p[i - 1] * 2 % mod
+for _ in range(q):
+    query = sys.stdin.readline().strip().split()
+    query_type = query[0]
+    k = int(query[1])
+    if query_type == '<':
+        i = bisect(elements, k - 1)
+        print(p[i] % mod)
+    elif query_type == '=':
+        i = bisect(elements, k)
+        j = bisect(elements, k - 1)
+        print((p[i] - p[j]) % mod)
+    else:
+        i = bisect(elements, k)
+        print((p[n] - p[i]) % mod)
