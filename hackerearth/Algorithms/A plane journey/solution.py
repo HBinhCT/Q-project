91cf94a9e2b67adf134beaaae264b9e699b84aac
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-10-02 15:47:21 +0700

    [ADD] A plane journey

---
+++ hackerearth/Algorithms/A plane journey/solution.py
@@ -0,0 +1,25 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+n, m = map(int, input().strip().split())
+a = sorted(map(int, input().strip().split()), reverse=True)
+b = sorted(map(int, input().strip().split()), reverse=True)
+if a[0] > b[0]:
+    print(-1)
+else:
+    min_time = 1
+    i = j = 0
+    while i < len(a):
+        if j < len(b) and a[i] <= b[j]:
+            j += 1
+        elif a[i] <= b[j - 1]:
+            min_time += 2
+        i += 1
+    print(min_time)
