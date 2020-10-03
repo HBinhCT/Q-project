9ff872130b6a93072f36168be7bfa03addeea46f
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-10-03 19:38:36 +0700

    [ADD] Wet Shark and Two Subsequences

---
+++ hackerrank/Algorithms/Wet Shark and Two Subsequences/solution.py
@@ -0,0 +1,54 @@
+#!/bin/python3
+
+import os
+import sys
+
+
+#
+# Complete the twoSubsequences function below.
+#
+def twoSubsequences(x, r, s):
+    #
+    # Write your code here.
+    #
+    if r < s or (r + s) % 2 or (r == 0 and s == 0):
+        return 0
+    mod = 1000000007  # 10 ** 9 + 7
+    ln = len(x)
+    dp = [[0] * (ln + 1) for _ in range(r + 1)]
+    dp[0][0] = 1
+    sum_a = (r + s) // 2
+    sum_b = (r - s) // 2
+    if x[0] <= sum_a:
+        dp[x[0]][1] = 1
+    for i in range(1, ln):
+        for j in range(sum_a, 0, -1):
+            for k in range(1, ln + 1):
+                if j >= x[i]:
+                    dp[j][k] += dp[j - x[i]][k - 1]
+                    dp[j][k] %= mod
+    ans = 0
+    for i in range(1, ln + 1):
+        ans += dp[sum_a][i] * dp[sum_b][i]
+        ans %= mod
+    return ans
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    mrs = input().split()
+
+    m = int(mrs[0])
+
+    r = int(mrs[1])
+
+    s = int(mrs[2])
+
+    x = list(map(int, input().rstrip().split()))
+
+    result = twoSubsequences(x, r, s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
---
+++ hackerrank/Algorithms/Wet Shark and Two Subsequences/test.py
@@ -0,0 +1,11 @@
+import unittest
+import solution
+
+
+class TestQ(unittest.TestCase):
+    def test_case_0(self):
+        self.assertEqual(solution.twoSubsequences([1, 1, 1, 4], 5, 3), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
