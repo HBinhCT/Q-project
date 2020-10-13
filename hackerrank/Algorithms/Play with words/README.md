2f7423ab77df7bd9f60b8400a9eefada1073a59c
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-10-13 20:50:42 +0700

    [ADD] Play with words

---
+++ hackerrank/Algorithms/Play with words/README.md
@@ -0,0 +1,4 @@
+https://www.hackerrank.com/challenges/strplay/problem
+
+Explain:
+- https://www.hackerrank.com/rest/contests/master/challenges/strplay/hackers/xilatech/download_solution
---
+++ hackerrank/Algorithms/Play with words/solution.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import os
+import sys
+
+
+#
+# Complete the playWithWords function below.
+#
+def playWithWords(s):
+    #
+    # Write your code here.
+    #
+    n = len(s)
+    dp = [[0] * n for _ in range(n)]
+    for i in range(n):
+        dp[i][i] = 1
+    for i in range(1, n):
+        for j in range(n - i):
+            if s[j] == s[j + i]:
+                dp[j][j + i] = dp[j + 1][j + i - 1] + 2
+            else:
+                dp[j][j + i] = max(dp[j + 1][j + i], dp[j][j + i - 1])
+    ans = 0
+    for i in range(n - 1):
+        ans = max(ans, dp[0][i] * dp[i + 1][n - 1])
+    return ans
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = playWithWords(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
