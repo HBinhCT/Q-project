85715f27527c8debc11c438fb890a702c0f45690
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-10-15 20:55:57 +0700

    [ADD] Interval Selection

---
+++ hackerrank/Algorithms/Interval Selection/solution.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import os
+import sys
+
+
+#
+# Complete the intervalSelection function below.
+#
+def intervalSelection(intervals):
+    #
+    # Write your code here.
+    #
+    sorted_intervals = sorted(intervals, key=lambda x: x[1])
+    count = 0
+    check = [0, 0]
+    for a, b in sorted_intervals:
+        if a > check[1]:
+            count += 1
+            check[1] = b
+        elif a > check[0]:
+            count += 1
+            check[0], check[1] = check[1], b
+    return count
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = int(input())
+
+    for s_itr in range(s):
+        n = int(input())
+
+        intervals = []
+
+        for _ in range(n):
+            intervals.append(list(map(int, input().rstrip().split())))
+
+        result = intervalSelection(intervals)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
---
+++ hackerrank/Algorithms/Interval Selection/test.py
@@ -0,0 +1,14 @@
+import unittest
+import solution
+
+
+class TestQ(unittest.TestCase):
+    def test_case_0(self):
+        self.assertEqual(solution.intervalSelection([[1, 2], [2, 3], [2, 4]]), 2)
+        self.assertEqual(solution.intervalSelection([[1, 5], [1, 5], [1, 5]]), 2)
+        self.assertEqual(solution.intervalSelection([[1, 10], [1, 3], [4, 6], [7, 10]]), 4)
+        self.assertEqual(solution.intervalSelection([[1, 10], [1, 3], [3, 6], [7, 10]]), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
