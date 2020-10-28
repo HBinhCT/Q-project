9f76f7c78c1e67764e810f81678c136ac86a903e
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-10-28 20:41:24 +0700

    [ADD] Sum vs XOR

---
+++ hackerrank/Algorithms/Sum vs XOR/README.md
@@ -0,0 +1,4 @@
+https://www.hackerrank.com/challenges/sum-vs-xor/problem
+
+Explain:
+- https://www.hackerrank.com/challenges/sum-vs-xor/forum/comments/200500
---
+++ hackerrank/Algorithms/Sum vs XOR/test.py
@@ -0,0 +1,14 @@
+import unittest
+import solution
+
+
+class TestQ(unittest.TestCase):
+    def test_case_0(self):
+        self.assertEqual(solution.sumXor(5), 2)
+
+    def test_case_1(self):
+        self.assertEqual(solution.sumXor(10), 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
