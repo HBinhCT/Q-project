4bd396dbe821f6d36bb9a0d5254213697801ce16
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-11-01 20:25:09 +0700

    [ADD] A Chessboard Game

---
+++ hackerrank/Algorithms/A Chessboard Game/README.md
@@ -0,0 +1,4 @@
+https://www.hackerrank.com/challenges/a-chessboard-game-1/problem
+
+Explain:
+- https://www.hackerrank.com/challenges/a-chessboard-game-1/forum/comments/140139
---
+++ hackerrank/Algorithms/A Chessboard Game/test.py
@@ -0,0 +1,18 @@
+import unittest
+import solution
+
+
+class TestQ(unittest.TestCase):
+    def test_case_0(self):
+        self.assertEqual(solution.chessboardGame(5, 2), 'Second')
+        self.assertEqual(solution.chessboardGame(5, 3), 'First')
+        self.assertEqual(solution.chessboardGame(8, 8), 'First')
+
+    def test_case_1(self):
+        self.assertEqual(solution.chessboardGame(7, 3), 'First')
+        self.assertEqual(solution.chessboardGame(8, 12), 'First')
+        self.assertEqual(solution.chessboardGame(9, 7), 'First')
+
+
+if __name__ == '__main__':
+    unittest.main()
