352df303070a1e2c3174c0799fe60b997a32f325
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-11-06 22:53:05 +0700

    [ADD] XOR Strings

---
+++ hackerrank/Algorithms/XOR Strings/README.md
@@ -0,0 +1,4 @@
+https://www.hackerrank.com/challenges/strings-xor/problem
+
+Explain:
+- https://www.hackerrank.com/challenges/strings-xor/forum/comments/839681
---
+++ hackerrank/Algorithms/XOR Strings/test.py
@@ -0,0 +1,20 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+from unittest.mock import patch
+
+
+class TestQ(unittest.TestCase):
+    @patch('builtins.input', side_effect=[
+        '10101',
+        '00101',
+    ])
+    def test_case_0(self, input_mock=None):
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            import solution
+        self.assertEqual(text_trap.getvalue(), '10000\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
