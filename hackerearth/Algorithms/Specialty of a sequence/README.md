c5aad178f85ec94808614abbbe36de95af578276
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2020-12-22 21:16:58 +0700

    [ADD] Specialty of a sequence

---
+++ hackerearth/Algorithms/Specialty of a sequence/README.md
@@ -0,0 +1,1 @@
+https://www.hackerearth.com/practice/algorithms/sorting/quick-sort/practice-problems/algorithm/lex-finds-beauty-0d0bc1b6/
---
+++ hackerearth/Algorithms/Specialty of a sequence/solution.py
@@ -0,0 +1,14 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+a.sort()
+print(sum(a[:n - k]))
