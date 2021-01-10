9e472520c71e7213db90dd563acb566f89441a8d
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-01-10 19:47:27 +0700

    [ADD] Divide Apples

---
+++ hackerearth/Algorithms/Divide Apples/solution.py
@@ -0,0 +1,22 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+n = int(input())
+apples = list(map(int, input().split()))
+avg = sum(apples) // n
+calc = [0] * n
+for i in range(n - 1):
+    calc[i + 1] = calc[i] + apples[i] - avg
+calc = sorted(calc)
+med = -calc[n // 2]
+ans = 0
+for i in range(n):
+    ans += abs(med + calc[i])
+print(ans)
