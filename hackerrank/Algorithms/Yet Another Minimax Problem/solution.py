499fefe047a31f9fbeb6d669d0730a8a8b81feaf
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-01-20 19:23:09 +0700

    [ADD] Yet Another Minimax Problem

---
+++ hackerrank/Algorithms/Yet Another Minimax Problem/solution.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the anotherMinimaxProblem function below.
+def anotherMinimaxProblem(a):
+    if sum(a) == 0:
+        return 0
+    bits = un_bits = 0
+    for i in a:
+        bits |= i
+        un_bits |= ~i
+    j = bits & un_bits
+    while j & (j - 1):
+        j &= j - 1
+    xs = [i for i in a if i & j]
+    ys = [i for i in a if i & j == 0]
+    return min(x ^ y for x in xs for y in ys)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = anotherMinimaxProblem(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
---
+++ hackerrank/Algorithms/Yet Another Minimax Problem/test.py
@@ -0,0 +1,14 @@
+import unittest
+import solution
+
+
+class TestQ(unittest.TestCase):
+    def test_case_0(self):
+        self.assertEqual(solution.anotherMinimaxProblem([1, 2, 3, 4]), 5)
+
+    def test_case_1(self):
+        self.assertEqual(solution.anotherMinimaxProblem([1, 3, 2]), 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
