421193d316059c660b63a72458aaa97dcecba824
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-01-22 21:53:10 +0700

    [ADD] Providing gifts

---
+++ hackerearth/Algorithms/Providing gifts/README.md
@@ -0,0 +1,1 @@
+https://www.hackerearth.com/practice/algorithms/greedy/basics-of-greedy-algorithms/practice-problems/algorithm/monotization-065bcc29/
---
+++ hackerearth/Algorithms/Providing gifts/solution.py
@@ -0,0 +1,14 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    print((n + 1) // 2)
