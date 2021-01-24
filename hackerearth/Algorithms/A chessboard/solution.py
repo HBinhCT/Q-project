d1a86ea4cd6c20d8aaea6c08c0dbb2e90c111f81
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-01-24 10:41:14 +0700

    [ADD] A chessboard

---
+++ hackerearth/Algorithms/A chessboard/solution.py
@@ -0,0 +1,20 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+t = int(input())
+for _ in range(t):
+    x1, y1 = map(int, input().strip().split())
+    x2, y2 = map(int, input().strip().split())
+    if x1 == x2 and y1 == y2:
+        print('SECOND')
+    elif max(x1, x2) - min(x1, x2) in (0, 1) and max(y1, y2) - min(y1, y2) in (0, 1):
+        print('FIRST')
+    else:
+        print('DRAW')
