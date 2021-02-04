8132527980fba77dad28a53eca9646b88ab12cd9
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-02-04 19:34:45 +0700

    [ADD] Stone Division, Revisited

---
+++ hackerrank/Algorithms/Stone Division, Revisited/README.md
@@ -0,0 +1,6 @@
+https://www.hackerrank.com/challenges/stone-division-2/problem
+
+Explain:
+- https://www.hackerrank.com/challenges/stone-division-2/forum/comments/793172
+- https://www.hackerrank.com/challenges/stone-division-2/forum/comments/424784
+- https://www.hackerrank.com/challenges/stone-division-2/forum/comments/451718
---
+++ hackerrank/Algorithms/Stone Division, Revisited/solution.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the stoneDivision function below.
+def stoneDivision(n, s):
+    from functools import lru_cache
+
+    @lru_cache(maxsize=None)
+    def moves(size):
+        next_moves = [0]
+        for i in s:
+            if size > i and size % i == 0:
+                next_moves.append(1 + size // i * moves(i))
+        return max(next_moves)
+
+    return moves(n)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        nm = input().split()
+
+        n = int(nm[0])
+
+        m = int(nm[1])
+
+        s = list(map(int, input().rstrip().split()))
+
+        result = stoneDivision(n, s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
