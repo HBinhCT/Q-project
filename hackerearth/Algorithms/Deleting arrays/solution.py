dd510dcc987943a9e9ed5e43817e43551b7af441
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-02-06 16:48:55 +0700

    [ADD] Deleting arrays

---
+++ hackerearth/Algorithms/Deleting arrays/solution.py
@@ -0,0 +1,14 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().strip().split())
+    print((n + 2 * k) // (2 * k + 1))
