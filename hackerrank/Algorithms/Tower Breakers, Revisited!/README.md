60a5f16f640350ee138031e12714de8b76e8a557
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-02-09 17:20:20 +0700

    [ADD] Tower Breakers, Revisited!

---
+++ hackerrank/Algorithms/Tower Breakers, Revisited!/README.md
@@ -0,0 +1,5 @@
+https://www.hackerrank.com/challenges/tower-breakers-revisited-1/problem
+
+Explain:
+- https://www.hackerrank.com/challenges/tower-breakers-revisited-1/forum/comments/770628
+- https://www.hackerrank.com/challenges/tower-breakers-revisited-1/forum/comments/860856
---
+++ hackerrank/Algorithms/Tower Breakers, Revisited!/solution.py
@@ -0,0 +1,51 @@
+#!/bin/python3
+
+import os
+import sys
+import math
+from functools import lru_cache
+
+
+@lru_cache(maxsize=None)
+def prime_factors(n):
+    count = 0
+    while n % 2 == 0:
+        count += 1
+        n = n / 2
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        while n % i == 0:
+            count += 1
+            n = n / i
+    if n > 2:
+        count += 1
+    return count
+
+
+#
+# Complete the towerBreakers function below.
+#
+def towerBreakers(arr):
+    #
+    # Write your code here.
+    #
+    d = 0
+    for x in arr:
+        d ^= prime_factors(x)
+    return 2 - (d != 0)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        arr_count = int(input())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = towerBreakers(arr)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
