972b8b0a21510580a2247987a09873eb16b924ea
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-02-10 13:52:01 +0700

    [ADD] Tower Breakers, Again!

---
+++ hackerrank/Algorithms/Tower Breakers, Again!/README.md
@@ -0,0 +1,4 @@
+https://www.hackerrank.com/challenges/tower-breakers-again-1/problem
+
+Explain:
+- https://www.hackerrank.com/challenges/tower-breakers-again-1/forum/comments/861138
---
+++ hackerrank/Algorithms/Tower Breakers, Again!/test.py
@@ -0,0 +1,12 @@
+import unittest
+import solution
+
+
+class TestQ(unittest.TestCase):
+    def test_case_0(self):
+        self.assertEqual(solution.towerBreakers([1, 2]), 1)
+        self.assertEqual(solution.towerBreakers([1, 2, 3]), 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
