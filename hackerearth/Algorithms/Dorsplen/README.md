00ce8255ba45ec63c9fa9bbeb6b602362a9e6afd
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-02-14 14:13:36 +0700

    [ADD] Dorsplen

---
+++ hackerearth/Algorithms/Dorsplen/README.md
@@ -0,0 +1,4 @@
+https://www.hackerearth.com/practice/algorithms/greedy/basics-of-greedy-algorithms/practice-problems/algorithm/dorsplen/
+
+Explain:
+- https://www.hackerearth.com/practice/algorithms/greedy/basics-of-greedy-algorithms/practice-problems/algorithm/dorsplen/editorial/
---
+++ hackerearth/Algorithms/Dorsplen/solution.py
@@ -0,0 +1,16 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+r, g, b = map(int, input().strip().split())
+m = min(r, g, b)
+r -= m
+g -= m
+b -= m
+print((r + g + b + 1) // 2 + m)
