b001e120c00dc472741f04c919d802c8e64ae2da
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-02-15 18:20:27 +0700

    [ADD] Unittest

---
+++ hackerrank/Python/Integers Come In All Sizes/test.py
@@ -0,0 +1,22 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+from unittest.mock import patch
+
+
+class TestQ(unittest.TestCase):
+    @patch('builtins.input', side_effect=[
+        '9',
+        '29',
+        '7',
+        '27',
+    ])
+    def test_case_0(self, input_mock=None):
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            import solution
+        self.assertEqual(text_trap.getvalue(), '4710194409608608369201743232\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
