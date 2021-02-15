c13667a49d59acb67be8b96bf4bbcfdbd4bfcc3f
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-02-15 17:00:01 +0700

    [ADD] Unittest

---
+++ hackerrank/Python/Inner and Outer/test.py
@@ -0,0 +1,23 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+from unittest.mock import patch
+
+
+class TestQ(unittest.TestCase):
+    @patch('builtins.input', side_effect=[
+        '0 1',
+        '2 3',
+    ])
+    def test_case_0(self, input_mock=None):
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            import solution
+        self.assertEqual(text_trap.getvalue(),
+                         '3\n' +
+                         '[[0 0]\n' +
+                         ' [2 3]]\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
