2d943b97caa71e78ee1c561b1930db65381aa434
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-02-15 16:32:35 +0700

    [ADD] One Swap to Palindrome

---
+++ hackerearth/Algorithms/One Swap to Palindrome/README.md
@@ -0,0 +1,1 @@
+https://www.hackerearth.com/practice/algorithms/greedy/basics-of-greedy-algorithms/practice-problems/algorithm/one-swap-to-palindrome-d96d4e21/
---
+++ hackerearth/Algorithms/One Swap to Palindrome/solution.py
@@ -0,0 +1,29 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+t = int(input())
+for _ in range(t):
+    s = input()
+    ln = len(s)
+    mid = ln // 2
+    x = s[:mid]
+    y = s[mid + ln % 2:][::-1]
+    comp = tuple((x[i], y[i]) for i in range(mid) if x[i] != y[i])
+    if ln % 2:
+        z = s[mid]
+        if len(comp) == 1:
+            print('Yes' if z in comp[0] else 'No')
+            continue
+    if len(comp) == 2:
+        print('Yes' if comp[0] == comp[1] or comp[0] == comp[1][::-1] else 'No')
+    elif len(comp) == 0:
+        print('Yes')
+    else:
+        print('No')
