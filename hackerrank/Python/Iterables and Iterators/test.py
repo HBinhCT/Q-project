5871ce6849b17ed44187f58cc657ad5aaa94426b
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-02-15 18:22:39 +0700

    [ADD] Unittest

---
+++ hackerrank/Python/Iterables and Iterators/test.py
@@ -0,0 +1,21 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+from unittest.mock import patch
+
+
+class TestQ(unittest.TestCase):
+    @patch('builtins.input', side_effect=[
+        '4',
+        'a a c d',
+        '2',
+    ])
+    def test_case_0(self, input_mock=None):
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            import solution
+        self.assertEqual(text_trap.getvalue(), '0.8333333333333334\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
