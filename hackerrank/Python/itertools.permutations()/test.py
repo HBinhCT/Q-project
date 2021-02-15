16cf079bf33f8c6086de7c006c8972ba72c6dc3e
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-02-15 18:35:55 +0700

    [ADD] Unittest

---
+++ hackerrank/Python/itertools.permutations()/test.py
@@ -0,0 +1,29 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+from unittest.mock import patch
+
+
+class TestQ(unittest.TestCase):
+    @patch('builtins.input', return_value='HACK 2')
+    def test_case_0(self, input_mock=None):
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            import solution
+        self.assertEqual(text_trap.getvalue(),
+                         'AC\n' +
+                         'AH\n' +
+                         'AK\n' +
+                         'CA\n' +
+                         'CH\n' +
+                         'CK\n' +
+                         'HA\n' +
+                         'HC\n' +
+                         'HK\n' +
+                         'KA\n' +
+                         'KC\n' +
+                         'KH\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
