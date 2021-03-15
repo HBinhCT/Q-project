9602febb0f39916163c87af85d14456e3354bd7a
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-03-15 20:18:23 +0700

    [ADD] Cities and co-ordinates.

---
+++ hackerearth/Algorithms/Cities and co-ordinates/solution.py
@@ -0,0 +1,21 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    xs = []
+    ys = []
+    for i in range(n):
+        x, y = map(int, input().split())
+        xs.append(x)
+        ys.append(y)
+    res = max(max(xs) - min(xs), max(ys) - min(ys))
+    print(res * res)
