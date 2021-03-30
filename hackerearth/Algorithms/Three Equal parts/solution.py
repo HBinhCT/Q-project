355fed02647434d6baf4348934582bc80f96291c
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-03-30 19:18:43 +0700

    [ADD] Three Equal parts

---
+++ hackerearth/Algorithms/Three Equal parts/solution.py
@@ -0,0 +1,28 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = input().strip().split()
+    ones = [i for i in range(n) if a[i] == '1']
+    ln_ones = len(ones)
+    if not ln_ones:
+        print(0)
+    elif ln_ones % 3 != 0:
+        print(-1)
+    else:
+        end_zeros = n - ones[-1] - 1
+        first = ones[ln_ones // 3 - 1] + end_zeros + 1
+        second = ones[2 * ln_ones // 3 - 1] + end_zeros + 1
+        part_1 = int(''.join((a[:first])), 2)
+        part_2 = int(''.join((a[first:second])), 2)
+        part_3 = int(''.join((a[second:])), 2)
+        print(part_1 % 1000000007 if part_1 == part_2 == part_3 else -1)
