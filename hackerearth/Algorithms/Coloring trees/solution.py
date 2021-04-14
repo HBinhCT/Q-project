94035c0d3e295d6710fb3bf71620e7bbd06b795d
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-04-14 20:56:34 +0700

    [ADD] Coloring trees

---
+++ hackerearth/Algorithms/Coloring trees/solution.py
@@ -0,0 +1,40 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+from collections import defaultdict
+
+n, k = map(int, input().strip().split())
+graph = defaultdict(set)
+for i in range(n - 1):
+    u, v = map(int, input().strip().split())
+    graph[u].add(v)
+    graph[v].add(u)
+terminals = set(map(int, input().split()))
+if len(terminals) < 2:
+    print(0)
+else:
+    def dfs(city, adj, cache, crowded):
+        cache[city] = True
+        for connected_city in adj[city]:
+            if not cache[connected_city]:
+                res = dfs(connected_city, adj, cache, crowded)
+                if res and not crowded[city]:
+                    crowded[city] = True
+        return crowded[city]
+
+
+    visited = [False] * (n + 1)
+    crowded_cities = [False] * (n + 1)
+    for terminal in terminals:
+        crowded_cities[terminal] = True
+    for terminal in terminals:
+        if not visited[terminal]:
+            dfs(terminal, graph, visited, crowded_cities)
+    print(crowded_cities.count(True))
---
+++ hackerearth/Algorithms/Coloring trees/test.py
@@ -0,0 +1,24 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+from unittest.mock import patch
+
+
+class TestQ(unittest.TestCase):
+    @patch('builtins.input', side_effect=[
+        '5 3',
+        '1 2',
+        '1 3',
+        '2 4',
+        '2 5',
+        '1 4 5',
+    ])
+    def test_case_0(self, input_mock=None, inputs_mock=None):
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            import solution
+        self.assertEqual(text_trap.getvalue(), '4\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
