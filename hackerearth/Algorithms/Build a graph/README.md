d9d40601ecbf7b0518f58989133510b2678f2713
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-04-15 21:15:08 +0700

    [ADD] Build a graph

---
+++ hackerearth/Algorithms/Build a graph/README.md
@@ -0,0 +1,1 @@
+https://www.hackerearth.com/practice/algorithms/graphs/graph-representation/practice-problems/algorithm/build-a-graph-5f5c6b4a/
---
+++ hackerearth/Algorithms/Build a graph/solution.py
@@ -0,0 +1,12 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+n = int(input())
+print('No' if n < 12 else 'Yes')
