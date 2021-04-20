852a8bd75a8983b32fcd91ad1d3fff6bdb1f312e
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-04-20 21:07:00 +0700

    [ADD] Tree-Stock Market

---
+++ hackerearth/Algorithms/Tree-Stock Market/README.md
@@ -0,0 +1,1 @@
+https://www.hackerearth.com/practice/algorithms/graphs/graph-representation/practice-problems/algorithm/tree-stock-market-1-9872b56f/
---
+++ hackerearth/Algorithms/Tree-Stock Market/test.py
@@ -0,0 +1,31 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+from unittest.mock import patch
+
+
+class TestQ(unittest.TestCase):
+    @patch('builtins.input', side_effect=[
+        '6 3',
+        '1 2',
+        '1 3',
+        '2 4',
+        '2 5',
+        '5 6',
+        '2 1 2 1 1 2',
+        '2 2 2',
+        '1 2 3',
+        '3 1 4',
+    ])
+    def test_case_0(self, input_mock=None):
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            import solution
+        self.assertEqual(text_trap.getvalue(),
+                         '0\n' +
+                         '2\n' +
+                         '1\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
