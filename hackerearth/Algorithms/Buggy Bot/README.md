8ae0e9ed502b3c8df267f6c09f1b4842235b035b
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-04-21 21:23:27 +0700

    [ADD] Buggy Bot

---
+++ hackerearth/Algorithms/Buggy Bot/README.md
@@ -0,0 +1,1 @@
+https://www.hackerearth.com/practice/algorithms/graphs/graph-representation/practice-problems/algorithm/buggy-bot-d8f6eb53/
---
+++ hackerearth/Algorithms/Buggy Bot/test.py
@@ -0,0 +1,29 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+from unittest.mock import patch
+
+
+class TestQ(unittest.TestCase):
+    @patch('builtins.input', side_effect=[
+        '5 5 3',
+        '1 2',
+        '2 3',
+        '3 4',
+        '4 1',
+        '2 4',
+        '1 2',
+        '3 4',
+        '2 3',
+    ])
+    def test_case_0(self, input_mock=None):
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            import solution
+        self.assertEqual(text_trap.getvalue(),
+                         '2\n' +
+                         '3 4\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
