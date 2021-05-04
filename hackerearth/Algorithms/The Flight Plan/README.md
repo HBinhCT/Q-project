8777e421d5631b177fae8802655070830b901554
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-05-04 22:22:55 +0700

    [ADD] The Flight Plan

---
+++ hackerearth/Algorithms/The Flight Plan/README.md
@@ -0,0 +1,1 @@
+https://www.hackerearth.com/practice/algorithms/graphs/breadth-first-search/practice-problems/algorithm/traffic-light-2-ee27ba45/
---
+++ hackerearth/Algorithms/The Flight Plan/test.py
@@ -0,0 +1,27 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+from unittest.mock import patch
+
+
+class TestQ(unittest.TestCase):
+    @patch('builtins.input', side_effect=[
+        '5 5 3 5',
+        '1 2',
+        '1 3',
+        '2 4',
+        '1 4',
+        '2 5',
+        '1 5',
+    ])
+    def test_case_0(self, input_mock=None):
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            import solution
+        self.assertEqual(text_trap.getvalue(),
+                         '3\n' +
+                         '1 2 5\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
