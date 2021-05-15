439361f6767e272b3a7e1206005402c63cc18f7b
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-05-16 00:26:25 +0700

    [ADD] Big P and Party

---
+++ hackerearth/Algorithms/Big P and Party/solution.py
@@ -0,0 +1,38 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+from collections import defaultdict, deque
+
+
+def dance(adjacency, lvl):
+    q = deque([(0, 0)])
+    visited = {0: 0}
+    while q:
+        u, w = q.popleft()
+        for v in adjacency[u]:
+            if v not in visited:
+                visited[v] = 0
+                lvl[v] = w + 1
+                q.append((v, w + 1))
+
+
+a, b = map(int, input().strip().split())
+couples = defaultdict(list)
+luckiness = {}
+for _ in range(b):
+    x, y = map(int, input().strip().split())
+    couples[x].append(y)
+    couples[y].append(x)
+dance(couples, luckiness)
+for i in range(1, a):
+    if i in luckiness:
+        print(luckiness[i])
+    else:
+        print(-1)
---
+++ hackerearth/Algorithms/Big P and Party/test.py
@@ -0,0 +1,29 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+from unittest.mock import patch
+
+
+class TestQ(unittest.TestCase):
+    @patch('builtins.input', side_effect=[
+        '5 6',
+        '0 1',
+        '0 2',
+        '3 2',
+        '2 4',
+        '4 3',
+        '1 2',
+    ])
+    def test_case_0(self, input_mock=None):
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            import solution
+        self.assertEqual(text_trap.getvalue(),
+                         '1\n' +
+                         '1\n' +
+                         '2\n' +
+                         '2\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
