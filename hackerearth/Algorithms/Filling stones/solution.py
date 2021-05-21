5c232cc8911d5385648f8f276cf2a13606fdcb03
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-05-21 23:11:32 +0700

    [ADD] Filling stones

---
+++ hackerearth/Algorithms/Filling stones/solution.py
@@ -0,0 +1,12 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+n = int(input())
+print(0 if n > 2 else 1 if n == 2 else 2)
