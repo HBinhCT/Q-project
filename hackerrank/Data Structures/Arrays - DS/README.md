e9d1383e94be4a82ce4ee0754d4510de5c332029
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-06-13 15:51:36 +0700

    [ADD] Arrays - DS

---
+++ hackerrank/Data Structures/Arrays - DS/README.md
@@ -0,0 +1,4 @@
+https://www.hackerrank.com/challenges/arrays-ds/problem
+
+Explain:
+- https://www.hackerrank.com/challenges/arrays-ds/forum/comments/847019
---
+++ hackerrank/Data Structures/Arrays - DS/test.py
@@ -0,0 +1,11 @@
+import unittest
+import solution
+
+
+class TestQ(unittest.TestCase):
+    def test_case_0(self):
+        self.assertListEqual(solution.reverseArray([1, 4, 3, 2]), [2, 3, 4, 1])
+
+
+if __name__ == '__main__':
+    unittest.main()
