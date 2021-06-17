8429cc3c294dc5e1a74e1bce24e8506863a2acc1
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-06-17 22:33:54 +0700

    [ADD] Left Rotation

---
+++ hackerrank/Data Structures/Left Rotation/README.md
@@ -0,0 +1,4 @@
+https://www.hackerrank.com/challenges/array-left-rotation/problem
+
+Explain:
+- https://www.hackerrank.com/challenges/array-left-rotation/forum/comments/296304
---
+++ hackerrank/Data Structures/Left Rotation/test.py
@@ -0,0 +1,11 @@
+import unittest
+import solution
+
+
+class TestQ(unittest.TestCase):
+    def test_case_0(self):
+        self.assertListEqual(solution.rotateLeft(4, [1, 2, 3, 4, 5]), [5, 1, 2, 3, 4])
+
+
+if __name__ == '__main__':
+    unittest.main()
