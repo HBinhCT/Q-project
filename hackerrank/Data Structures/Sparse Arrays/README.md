f6650bd36f59d30fd7d80c824cc6f95f8dc93406
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-06-18 23:20:03 +0700

    [ADD] Sparse Arrays

---
+++ hackerrank/Data Structures/Sparse Arrays/README.md
@@ -0,0 +1,4 @@
+https://www.hackerrank.com/challenges/sparse-arrays/problem
+
+Explain:
+- https://www.hackerrank.com/challenges/sparse-arrays/forum/comments/145968
---
+++ hackerrank/Data Structures/Sparse Arrays/test.py
@@ -0,0 +1,40 @@
+import unittest
+import solution
+
+
+class TestQ(unittest.TestCase):
+    def test_case_0(self):
+        self.assertListEqual(solution.matchingStrings(
+            ['aba', 'baba', 'aba', 'xzxb'],
+            ['aba', 'xzxb', 'ab'],
+        ), [2, 1, 0])
+
+    def test_case_1(self):
+        self.assertListEqual(solution.matchingStrings(
+            ['def', 'de', 'fgh'],
+            ['de', 'lmn', 'fgh'],
+        ), [1, 0, 1])
+
+    def test_case_2(self):
+        self.assertListEqual(solution.matchingStrings(
+            [
+                'abcde',
+                'sdaklfj',
+                'asdjf',
+                'na',
+                'basdn',
+                'sdaklfj',
+                'asdjf',
+                'na',
+                'asdjf',
+                'na',
+                'basdn',
+                'sdaklfj',
+                'asdjf',
+            ],
+            ['abcde', 'sdaklfj', 'asdjf', 'na', 'basdn'],
+        ), [1, 3, 4, 3, 2])
+
+
+if __name__ == '__main__':
+    unittest.main()
