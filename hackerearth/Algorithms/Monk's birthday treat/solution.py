e6810eb5c410e8f04f1748f1acb0b07c1e2d02a6
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-06-20 18:17:03 +0700

    [ADD] Monk's birthday treat

---
+++ hackerearth/Algorithms/Monk's birthday treat/solution.py
@@ -0,0 +1,36 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+from collections import defaultdict
+
+
+def invite(node, adjacency):
+    visited = {node}
+    stack = [node]
+    count = 0
+    while stack:
+        aux = stack.pop()
+        count += 1
+        for vertex in adjacency[aux]:
+            if vertex not in visited:
+                visited.add(vertex)
+                stack.append(vertex)
+    return count
+
+
+n, d = map(int, input().strip().split())
+friends = defaultdict(set)
+for _ in range(d):
+    a, b = map(int, input().strip().split())
+    friends[a].add(b)
+ans = float('inf')
+for i in range(1, n + 1):
+    ans = min(ans, invite(i, friends))
+print(ans)
