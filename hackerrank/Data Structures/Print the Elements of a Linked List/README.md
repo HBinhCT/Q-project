e098d42341bd018269f28ddeaa88b4331299cad6
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-06-20 15:33:14 +0700

    [ADD] Print the Elements of a Linked List

---
+++ hackerrank/Data Structures/Print the Elements of a Linked List/README.md
@@ -0,0 +1,4 @@
+https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list/problem
+
+Explain:
+- https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list/forum/comments/157365
---
+++ hackerrank/Data Structures/Print the Elements of a Linked List/test.py
@@ -0,0 +1,36 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+import solution
+
+
+class TestQ(unittest.TestCase):
+    def test_case_0(self):
+        linked_list = solution.SinglyLinkedList()
+        linked_list.insert_node(16)
+        linked_list.insert_node(13)
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            solution.printLinkedList(linked_list.head)
+        self.assertEqual(text_trap.getvalue(),
+                         '16\n' +
+                         '13\n')
+
+    def test_case_1(self):
+        linked_list = solution.SinglyLinkedList()
+        linked_list.insert_node(17)
+        linked_list.insert_node(19)
+        linked_list.insert_node(5)
+        linked_list.insert_node(12)
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            solution.printLinkedList(linked_list.head)
+        self.assertEqual(text_trap.getvalue(),
+                         '17\n' +
+                         '19\n' +
+                         '5\n' +
+                         '12\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
