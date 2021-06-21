e0f7287908266ac8f1196f44ecaa6fd9ec022ac1
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-06-21 22:34:59 +0700

    [ADD] Insert a Node at the Tail of a Linked List

---
+++ hackerrank/Data Structures/Insert a Node at the Tail of a Linked List/test.py
@@ -0,0 +1,34 @@
+import unittest
+import solution
+
+
+class TestQ(unittest.TestCase):
+    def test_case_0(self):
+        inputs = [141, 302, 164, 530, 474]
+        outputs = [141, 302, 164, 530, 474]
+        linked_list = solution.SinglyLinkedList()
+        for item in inputs:
+            linked_list.head = solution.insertNodeAtTail(linked_list.head, item)
+        node = linked_list.head
+        i = 0
+        while node:
+            self.assertEqual(node.data, outputs[i])
+            node = node.next
+            i += 1
+
+    def test_case_1(self):
+        inputs = [236, 326, 937]
+        outputs = [236, 326, 937]
+        linked_list = solution.SinglyLinkedList()
+        for item in inputs:
+            linked_list.head = solution.insertNodeAtTail(linked_list.head, item)
+        node = linked_list.head
+        i = 0
+        while node:
+            self.assertEqual(node.data, outputs[i])
+            node = node.next
+            i += 1
+
+
+if __name__ == '__main__':
+    unittest.main()
