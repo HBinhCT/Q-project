67b4e5c3d3454b8847d9813351f675ee7cc0a8c7
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-06-24 22:01:47 +0700

    [ADD] Delete a Node

---
+++ hackerrank/Data Structures/Delete a Node/README.md
@@ -0,0 +1,4 @@
+https://www.hackerrank.com/challenges/delete-a-node-from-a-linked-list/problem
+
+Explain:
+- https://www.hackerrank.com/challenges/delete-a-node-from-a-linked-list/forum/comments/781903
---
+++ hackerrank/Data Structures/Delete a Node/test.py
@@ -0,0 +1,34 @@
+import unittest
+import solution
+
+
+class TestQ(unittest.TestCase):
+    def test_case_0(self):
+        inputs = [20, 6, 2, 19, 7, 4, 15, 9]
+        outputs = [20, 6, 2, 7, 4, 15, 9]
+        linked_list = solution.SinglyLinkedList()
+        for item in inputs:
+            linked_list.insert_node(item)
+        node = solution.deleteNode(linked_list.head, 3)
+        i = 0
+        while node:
+            self.assertEqual(node.data, outputs[i])
+            node = node.next
+            i += 1
+
+    def test_case_1(self):
+        inputs = [11, 9, 2, 9]
+        outputs = [11, 2, 9]
+        linked_list = solution.SinglyLinkedList()
+        for item in inputs:
+            linked_list.insert_node(item)
+        node = solution.deleteNode(linked_list.head, 1)
+        i = 0
+        while node:
+            self.assertEqual(node.data, outputs[i])
+            node = node.next
+            i += 1
+
+
+if __name__ == '__main__':
+    unittest.main()
