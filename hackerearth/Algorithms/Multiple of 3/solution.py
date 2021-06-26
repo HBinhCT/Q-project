10479712571055f35b67cb336fad96cad9ebd8b9
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-06-26 19:59:22 +0700

    [ADD] Multiple of 3

---
+++ hackerearth/Algorithms/Multiple of 3/solution.py
@@ -0,0 +1,30 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+t = int(input())
+for _ in range(t):
+    n = input()
+    digits = list(map(int, n))
+    s = sum(digits)
+    if not s % 3:
+        print(n)
+    else:
+        x = s // 3 * 3 + 3 - s
+        idx = None
+        for i in range(len(n)):
+            if digits[i] > x:
+                idx = i - 1
+                break
+            idx = i + 1
+        if idx == -1:
+            n = str(x) + n
+        else:
+            n = n[:idx + 1] + str(x) + n[idx + 1:]
+        print(n)
