9df7a3b01d5f58b762d235c34590906681a89555
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-06-29 22:00:58 +0700

    [ADD] Get Node Value

---
+++ hackerrank/Data Structures/Get Node Value/test.py
@@ -0,0 +1,24 @@
+import unittest
+import solution
+
+
+class TestQ(unittest.TestCase):
+    def test_case_0(self):
+        linked_list = solution.SinglyLinkedList()
+        for item in [1]:
+            linked_list.insert_node(item)
+        self.assertEqual(solution.getNode(linked_list.head, 0), 1)
+        linked_list = solution.SinglyLinkedList()
+        for item in [3, 2, 1]:
+            linked_list.insert_node(item)
+        self.assertEqual(solution.getNode(linked_list.head, 2), 3)
+
+    def test_case_1(self):
+        linked_list = solution.SinglyLinkedList()
+        for item in [4, 3, 2, 1]:
+            linked_list.insert_node(item)
+        self.assertEqual(solution.getNode(linked_list.head, 2), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
