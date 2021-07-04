e69b522320bc9cc071f70a34d1addf834cfbf617
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-07-04 14:38:00 +0700

    [ADD] Reverse a doubly linked list

---
+++ hackerrank/Data Structures/Reverse a doubly linked list/README.md
@@ -0,0 +1,4 @@
+https://www.hackerrank.com/challenges/reverse-a-doubly-linked-list/problem
+
+Explain:
+- https://www.hackerrank.com/challenges/reverse-a-doubly-linked-list/forum/comments/277766
---
+++ hackerrank/Data Structures/Reverse a doubly linked list/test.py
@@ -0,0 +1,47 @@
+import unittest
+import solution
+
+
+class TestQ(unittest.TestCase):
+    def test_case_0(self):
+        inputs = [1, 2, 3, 4]
+        outputs = [4, 3, 2, 1]
+        linked_list = solution.DoublyLinkedList()
+        for item in inputs:
+            linked_list.insert_node(item)
+        node = solution.reverse(linked_list.head)
+        i = 0
+        while node:
+            self.assertEqual(node.data, outputs[i])
+            node = node.next
+            i += 1
+
+    def test_case_1(self):
+        inputs = [2, 3, 4]
+        outputs = [4, 3, 2]
+        linked_list = solution.DoublyLinkedList()
+        for item in inputs:
+            linked_list.insert_node(item)
+        node = solution.reverse(linked_list.head)
+        i = 0
+        while node:
+            self.assertEqual(node.data, outputs[i])
+            node = node.next
+            i += 1
+
+    def test_case_2(self):
+        inputs = [17, 20, 23, 35, 47]
+        outputs = [47, 35, 23, 20, 17]
+        linked_list = solution.DoublyLinkedList()
+        for item in inputs:
+            linked_list.insert_node(item)
+        node = solution.reverse(linked_list.head)
+        i = 0
+        while node:
+            self.assertEqual(node.data, outputs[i])
+            node = node.next
+            i += 1
+
+
+if __name__ == '__main__':
+    unittest.main()
