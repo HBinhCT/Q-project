58478e2ff260752a92c5c6c16fc62507b8772323
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-08-29 16:44:24 +0700

    [ADD] Region Find

---
+++ hackerearth/Algorithms/Region Find/test.py
@@ -0,0 +1,32 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+from unittest.mock import patch
+
+
+class TestQ(unittest.TestCase):
+    @patch('builtins.input', side_effect=[
+        '4',
+        '2 4 3 1 ',
+        '4 3',
+        '4 2',
+        '4 1',
+        '4',
+        '1',
+        '2',
+        '3',
+        '4',
+    ])
+    def test_case_0(self, input_mock=None):
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            import solution
+        self.assertEqual(text_trap.getvalue(),
+                         '8\n' +
+                         '1\n' +
+                         '1\n' +
+                         '1\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
