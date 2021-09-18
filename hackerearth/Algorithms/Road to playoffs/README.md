6300a9eb93654f4df7e668dc5c9f73001dc9a9e0
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-09-18 19:53:51 +0700

    [ADD] Road to playoffs

---
+++ hackerearth/Algorithms/Road to playoffs/README.md
@@ -0,0 +1,6 @@
+https://www.hackerearth.com/practice/algorithms/searching/binary-search/practice-problems/algorithm/road-to-playoffs-4e5d8318/
+
+Explain:
+
+- https://www.hackerearth.com/practice/algorithms/searching/binary-search/practice-problems/algorithm/road-to-playoffs-4e5d8318/editorial/
+- https://www.hackerearth.com/submission/62268041/
---
+++ hackerearth/Algorithms/Road to playoffs/solution.py
@@ -0,0 +1,29 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+t = int(input())
+for _ in range(t):
+    n, m, k, b = map(int, input().strip().split())
+    x = sorted(map(int, input().strip().split()), reverse=True)
+    left = b - 1
+    right = n - 1
+    while left < right:
+        mid = right - (right - left) // 2  # don't use (right + left) // 2
+        if x[b - 1] > x[mid] + m:
+            right = mid - 1
+        else:
+            cur = 0
+            for i in range(b - 1, mid):
+                cur += x[mid] + m - x[i]
+            if cur < m * (k - (b - 1) - (n - mid)):
+                right = mid - 1
+            else:
+                left = mid
+    print(left + 1)
