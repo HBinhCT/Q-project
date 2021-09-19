309842e5047c23a00a019a936da6a419eb08f74f
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-09-19 14:15:29 +0700

    [ADD] A special sequence

---
+++ hackerearth/Algorithms/A special sequence/README.md
@@ -0,0 +1,1 @@
+https://www.hackerearth.com/practice/algorithms/searching/binary-search/practice-problems/algorithm/special-sequence-1cbd35f9/
---
+++ hackerearth/Algorithms/A special sequence/solution.py
@@ -0,0 +1,40 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+from bisect import bisect
+from math import floor, sqrt, ceil
+
+
+def generate_array(max_val):
+    def generate_number(x):
+        return x * floor(sqrt(x)) + ceil(x / 2)
+
+    arr = [1]
+    i = 0
+    val = 0
+    while val < max_val:
+        val = arr[-1] + generate_number(i)
+        arr.append(val)
+        i += 1
+    return arr
+
+
+q = int(input())
+queries = []
+size = 0
+for _ in range(q):
+    l, r = map(int, input().strip().split())
+    size = max(size, r)
+    queries.append((l, r))
+a = generate_array(size)
+for left, right in queries:
+    idx1 = bisect(a, left) - 1
+    idx2 = bisect(a, right) - 1
+    print(idx2 - idx1 + 1)
