3ee6dfe9edaa074231ebe8af480c7ad2dc8337a8
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-10-10 17:15:16 +0700

    [ADD] Remove Duplicates

---
+++ hackerearth/Algorithms/Remove Duplicates/solution.py
@@ -0,0 +1,16 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+s = input()
+ans = ''
+for c in s:
+    if c not in ans:
+        ans = ans + c
+print(ans)
