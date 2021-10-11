d1cfb55f373a1164bccaf504c090313d0143e7f8
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-10-11 21:24:54 +0700

    [ADD] Nobita and String

---
+++ hackerearth/Algorithms/Nobita and String/test.py
@@ -0,0 +1,20 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+from unittest.mock import patch
+
+
+class TestQ(unittest.TestCase):
+    @patch('builtins.input', side_effect=[
+        '1',
+        'hello world',
+    ])
+    def test_case_0(self, input_mock=None):
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            import solution
+        self.assertEqual(text_trap.getvalue(), 'world hello\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
