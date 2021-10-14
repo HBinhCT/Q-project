c8bb0a08593ccf54a5d6f79113edccee0f64b6f1
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-10-14 18:34:57 +0700

    [MOD] Update code

--- hackerearth/Algorithms/The Substring Game!/test.py
+++ hackerearth/Algorithms/The Substring Game!/test.py
@@ -10,7 +10,7 @@ class TestQ(unittest.TestCase):
         '2',
         '2 9',
     ])
-    def test_case_0(self, input_mock=None, inputs_mock=None):
+    def test_case_0(self, input_mock=None):
         text_trap = io.StringIO()
         with redirect_stdout(text_trap):
             import solution
