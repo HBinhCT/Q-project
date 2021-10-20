0941a29793b8f4ea287dbdd0a84b6b4d774b7856
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-10-20 22:26:46 +0700

    [ADD] A strongest string

---
+++ hackerearth/Algorithms/A strongest string/README.md
@@ -0,0 +1,1 @@
+https://www.hackerearth.com/practice/algorithms/string-algorithm/string-searching/practice-problems/algorithm/strongest-string-4103a929/
---
+++ hackerearth/Algorithms/A strongest string/solution.py
@@ -0,0 +1,23 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = input().strip()
+    ans = max(s)
+    idx = s.index(ans)
+    for i in range(ord(ans) - 1, ord('a') - 1, -1):
+        try:
+            idx = s.index(chr(i), idx + 1)
+            ans += chr(i)
+        except ValueError:
+            pass
+    print(ans)
