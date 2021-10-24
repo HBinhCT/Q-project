95d0e538ce84ac2f73cf03f16ebae0ba5367c722
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-10-24 13:42:48 +0700

    [ADD] String

---
+++ hackerearth/Algorithms/String/README.md
@@ -0,0 +1,1 @@
+https://www.hackerearth.com/practice/algorithms/string-algorithm/string-searching/practice-problems/algorithm/string-4-d1093b86/
---
+++ hackerearth/Algorithms/String/solution.py
@@ -0,0 +1,15 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+from collections import Counter
+
+n = int(input())
+s = input().strip()
+print(n - Counter(s).most_common(1)[0][-1])
