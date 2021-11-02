ac1071a96574b8f82a9da9d960c98cac61d1bdb9
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-11-02 22:22:24 +0700

    [ADD] ASCII Value

---
+++ hackerearth/Algorithms/ASCII Value/solution.py
@@ -0,0 +1,12 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+c = input().strip()
+print(ord(c))
