b3b081b5c47bc34ab769969c4990909b0d703e7f
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-11-20 23:27:24 +0700

    [ADD] Almost Same String

---
+++ hackerearth/Algorithms/Almost Same String/README.md
@@ -0,0 +1,1 @@
+https://www.hackerearth.com/practice/algorithms/string-algorithm/string-searching/practice-problems/algorithm/forming-almost-similar-string-d551fc3d/
---
+++ hackerearth/Algorithms/Almost Same String/solution.py
@@ -0,0 +1,34 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+from string import ascii_lowercase
+from collections import Counter
+
+
+def dist(c1, c2):
+    diff = abs(ord(c1) - ord(c2))
+    return min(diff, ord('z') - diff - ord('a') + 1)
+
+
+t = int(input())
+for _ in range(t):
+    a = input()
+    b = input()
+    k = int(input())
+    counter = Counter(a + b)
+    min_step = float('inf')
+    for start in ascii_lowercase:
+        end = chr((((ord(start) - ord('a')) + k) % 26) + ord('a'))
+        step = 0
+        for c in ascii_lowercase:
+            if (start <= end and (c < start or c > end)) or end < c < start:
+                step += counter[c] * min(dist(c, start), dist(c, end))
+        min_step = min(min_step, step)
+    print(min_step)
