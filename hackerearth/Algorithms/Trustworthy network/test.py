f5aee02b0ab4cdae5334b5e90fa45a89afe2d02b
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2021-12-15 23:54:22 +0700

    [ADD] Trustworthy network

---
+++ hackerearth/Algorithms/Trustworthy network/test.py
@@ -0,0 +1,24 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+from unittest.mock import patch
+
+
+class TestQ(unittest.TestCase):
+    @patch('builtins.input', side_effect=[
+        '4 5 1 3',
+        '1 2 10',
+        '2 3 5',
+        '2 4 7',
+        '3 2 1',
+        '4 1 2',
+    ])
+    def test_case_0(self, input_mock=None):
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            import solution
+        self.assertEqual(text_trap.getvalue(), '25\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
