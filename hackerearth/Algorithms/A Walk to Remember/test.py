50c76c49eebfced621492a18cf2e5a39f7546f01
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2022-01-01 19:44:00 +0700

    [ADD] A Walk to Remember

---
+++ hackerearth/Algorithms/A Walk to Remember/test.py
@@ -0,0 +1,24 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+from unittest.mock import patch
+
+
+class TestQ(unittest.TestCase):
+    @patch('sys.stdin.readline', side_effect=[
+        '5 5',
+        '1 2 ',
+        '2 3 ',
+        '3 4 ',
+        '4 5',
+        '4 2',
+    ])
+    def test_case_0(self, input_mock=None):
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            import solution
+        self.assertEqual(text_trap.getvalue(), '0 1 1 1 0\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
