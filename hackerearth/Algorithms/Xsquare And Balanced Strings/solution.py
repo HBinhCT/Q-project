1cf37eb884051c5b5254b9c96a154a294015b335
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2022-01-15 21:02:07 +0700

    [ADD] Xsquare And Balanced Strings

---
+++ hackerearth/Algorithms/Xsquare And Balanced Strings/solution.py
@@ -0,0 +1,19 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+t = int(input())
+for _ in range(t):
+    s = input()
+    for c in set(s):
+        if s.count(c) % 2:
+            print(-1)
+            break
+    else:
+        print(1)
