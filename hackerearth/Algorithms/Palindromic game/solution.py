41a8da77a35eead7a950e74acc415423950ec4a9
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2022-01-29 13:25:57 +0700

    [ADD] Palindromic game

---
+++ hackerearth/Algorithms/Palindromic game/solution.py
@@ -0,0 +1,38 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+s = input().strip()
+player = {
+    1: 'Alice',
+    2: 'Bob',
+}
+
+
+def play(string, start, end, box=None, turn=player[1]):
+    if box is None:
+        box = []
+    if start == end:
+        return turn if string[start] in box else 'Tie'
+    if string[start] in box or string[end] in box:
+        return turn
+    flip = player[2] if turn == player[1] else player[1]
+    if (string[start + 1] in box and string[end - 1] in box) or string[start] == string[end]:
+        return flip
+    if string[start] == string[start + 1]:
+        return play(string, start, end - 1, box + [string[end]], flip)
+    if string[end] == string[end - 1]:
+        return play(string, start + 1, end, box + [string[start]], flip)
+    if (string[start + 1] in box or turn == player[2]) and string[end - 1] not in box:
+        return play(string, start, end - 1, box + [string[end]], flip)
+    else:
+        return play(string, start + 1, end, box + [string[start]], flip)
+
+
+print(play(s, 0, len(s) - 1))
