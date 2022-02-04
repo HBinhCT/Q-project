e64f1eafbc23e232766d8aef43c925ced8311700
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2022-02-04 14:37:19 +0700

    [ADD] A Weird Game

---
+++ hackerearth/Algorithms/A Weird Game/README.md
@@ -0,0 +1,1 @@
+https://www.hackerearth.com/practice/algorithms/dynamic-programming/introduction-to-dynamic-programming-1/practice-problems/algorithm/a-weird-game-e2b9afe4/
---
+++ hackerearth/Algorithms/A Weird Game/solution.py
@@ -0,0 +1,34 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+from bisect import bisect_left
+
+q = int(input())
+for _ in range(q):
+    s = input().strip()
+    t = input().strip()
+    n = len(s)
+    m = len(t)
+    count = j = 0
+    dp = []
+    for i in range(n):
+        if j < m and s[i] == t[j]:
+            count += 1
+            j += 1
+        dp.append(count)
+    ans = 0
+    for i in range(n - 1, -1, -1):
+        idx = bisect_left(dp, m)
+        if m == 0:
+            idx = - 1
+        ans += i - idx
+        if m > 0 and s[i] == t[m - 1]:
+            m -= 1
+    print(ans)
