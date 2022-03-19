67c88f1724c32ab82d5804e2fdcdf62308af571a
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2022-03-19 12:53:33 +0700

    [ADD] Color the bricks

---
+++ hackerearth/Algorithms/Color the bricks/solution.py
@@ -0,0 +1,37 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+from functools import reduce
+from math import factorial
+
+mod = 1000000007
+n, k = list(map(int, input().strip().split()))
+bricks = sorted(map(int, input().strip().split()))
+if bricks[0] != 1:
+    bricks = [0] + bricks
+if bricks[-1] != n:
+    bricks.append(n + 1)
+counter = []
+for i in range(len(bricks) - 1):
+    count = bricks[i + 1] - bricks[i]
+    if count != 1:
+        counter.append(count - 1)
+x = reduce(lambda a, b: a * factorial(b) % mod, counter, 1)
+length = len(counter)
+total = sum(counter)
+if bricks[0] == 0:
+    length -= 1
+    total -= counter[0]
+if bricks[-1] == n + 1:
+    length -= 1
+    total -= counter[-1]
+res = factorial(sum(counter)) * pow(x, mod - 2, mod) * pow(2, total - length, mod)
+res %= mod
+print(res)
