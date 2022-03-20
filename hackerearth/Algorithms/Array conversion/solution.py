c5d1b048ddae023777e22abc6060f01d4ea3763b
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2022-03-20 12:47:56 +0700

    [ADD] Array conversion

---
+++ hackerearth/Algorithms/Array conversion/solution.py
@@ -0,0 +1,25 @@
+"""
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+"""
+
+# Write your code here
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().strip().split())
+    arr = list(map(int, input().strip().split()))
+    sn = n * (n + 1) // 2
+    total = sum(arr)
+    op_1 = sum(map(lambda x: -x, arr))
+    op_2 = total + sn
+    op_3 = total - sn
+    if op_1 < k < min(op_2, op_3) and op_2 > 250:
+        print('YES')
+    elif k > op_2 or k < op_3:
+        print('NO')
+    else:
+        print('YES')
