2b2af33dd65ada699814b38f2421a684ba79b35d
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2022-03-20 12:28:59 +0700

    [ADD] Bob and Forest

---
+++ hackerearth/Algorithms/Bob and Forest/README.md
@@ -0,0 +1,1 @@
+https://www.hackerearth.com/practice/algorithms/dynamic-programming/2-dimensional/practice-problems/algorithm/bob-and-forest-2571cd1f/
---
+++ hackerearth/Algorithms/Bob and Forest/test.py
@@ -0,0 +1,30 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+from unittest.mock import patch
+
+
+class TestQ(unittest.TestCase):
+    @patch('builtins.input', side_effect=[
+        '4 4',
+        '*..*',
+        '.***',
+        '****',
+        '.***',
+        '3',
+        '1',
+        '2',
+        '3',
+    ])
+    def test_case_0(self, input_mock=None):
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            import solution
+        self.assertEqual(text_trap.getvalue(),
+                         '12\n' +
+                         '16\n' +
+                         '17\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
