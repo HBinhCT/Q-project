c406746a3d691e96d845520e5cdac3d60c2ed839
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2022-04-07 20:33:12 +0700

    [ADD] The Final Confusion

---
+++ hackerearth/Algorithms/The Final Confusion/test.py
@@ -0,0 +1,23 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+from unittest.mock import patch
+
+
+class TestQ(unittest.TestCase):
+    @patch('builtins.input', side_effect=[
+        '2',
+        '1',
+        '3',
+    ])
+    def test_case_0(self, input_mock=None):
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            import solution
+        self.assertEqual(text_trap.getvalue(),
+                         '1\n' +
+                         '5\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
