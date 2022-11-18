8b82617c7a4cd720efec71e1d5850feecfd6bcfc
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2022-11-18 23:47:16 +0700

    [ADD] Panda and Chain Reaction

---
+++ hackerearth/Math/Number Theory/Basic Number Theory-1/Panda and Chain Reaction/solution.py
@@ -0,0 +1,20 @@
+from math import factorial
+from sys import stdin
+
+mod = 1000003
+
+
+def fac(arr, i):
+    ln = len(arr)
+    if i < ln:
+        return arr[i]
+    for i in range(ln, i + 1):
+        arr.append(arr[i - 1] * i % mod)
+    return arr[-1]
+
+
+t = int(stdin.readline())
+comp = [1, 1]
+for _ in range(t):
+    n, x = map(int, stdin.readline().strip().split())
+    print(0 if x >= mod else fac(comp, n) * x % mod)
