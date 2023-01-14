7d0c52e6fc9b66d40c3d0b24528ed2b71e4e6298
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2023-01-14 23:30:57 +0700

    [ADD] Fruit Slicing

---
+++ hackerearth/Algorithms/Greedy Algorithms/Basics of Greedy Algorithms/Fruit Slicing/test.py
@@ -0,0 +1,25 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+from unittest.mock import patch
+
+
+class TestQ(unittest.TestCase):
+    @patch('builtins.input', side_effect=[
+        '2',
+        '6',
+        '20 40 30 50 40 20',
+        '4',
+        '3 6 7 7',
+    ])
+    def test_case_0(self, input_mock=None):
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            import solution
+        self.assertEqual(text_trap.getvalue(),
+                         '4\n' +
+                         '3\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
