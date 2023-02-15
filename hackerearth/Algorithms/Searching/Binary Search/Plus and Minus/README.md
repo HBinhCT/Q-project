b1d5bac190af0d554b1f802fbba0c99acedd39b7
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2023-02-15 22:49:24 +0700

    [ADD] Plus and Minus

---
+++ hackerearth/Algorithms/Searching/Binary Search/Plus and Minus/README.md
@@ -0,0 +1,1 @@
+https://www.hackerearth.com/practice/algorithms/searching/binary-search/practice-problems/algorithm/plus-and-minus-0f3ab24f/
---
+++ hackerearth/Algorithms/Searching/Binary Search/Plus and Minus/test.py
@@ -0,0 +1,26 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+from unittest.mock import patch
+
+
+class TestQ(unittest.TestCase):
+    @patch('builtins.input', side_effect=[
+        '2',
+        '8',
+        '8 5 3 2 2 1 1 1',
+        '8',
+        '8 7 6 5 4 3 2 1',
+    ])
+    def test_case_0(self, input_mock=None):
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            import solution
+        self.assertEqual(text_trap.getvalue(),
+                         'YES\n' +
+                         '2 1\n' +
+                         'NO\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
