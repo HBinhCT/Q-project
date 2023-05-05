c2fcdbe9e01ac1c10bbfc1934339be629f87e7ee
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2023-05-05 22:51:08 +0700

    [ADD] Pile of Coins

---
+++ hackerearth/Basic Programming/Implementation/Basics of Implementation/Pile of Coins/README.md
@@ -0,0 +1,1 @@
+https://www.hackerearth.com/practice/basic-programming/implementation/basics-of-implementation/practice-problems/algorithm/pile-of-coins-d33de897/
---
+++ hackerearth/Basic Programming/Implementation/Basics of Implementation/Pile of Coins/test.py
@@ -0,0 +1,25 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+from unittest.mock import patch
+
+
+class TestQ(unittest.TestCase):
+    @patch('builtins.input', side_effect=[
+        '3',
+        '1 1',
+        '1 2',
+        '2 2',
+    ])
+    def test_case_0(self, input_mock=None):
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            import solution
+        self.assertEqual(text_trap.getvalue(),
+                         'Jeel\n' +
+                         'Ashish\n' +
+                         'Ashish\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
