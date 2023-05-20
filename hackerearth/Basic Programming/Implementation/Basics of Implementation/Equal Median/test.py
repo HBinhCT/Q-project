71cd99b8dac1d7348b200d4977c52d0a770e34f5
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2023-05-20 19:38:59 +0700

    [ADD] Equal Median

---
+++ hackerearth/Basic Programming/Implementation/Basics of Implementation/Equal Median/test.py
@@ -0,0 +1,22 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+from unittest.mock import patch
+
+
+class TestQ(unittest.TestCase):
+    @patch('builtins.input', side_effect=[
+        '1',
+        '7',
+        '1 2 3 3 5 6 7',
+        '4 6 8 8 9 9 9',
+    ])
+    def test_case_0(self, input_mock=None):
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            import solution
+        self.assertEqual(text_trap.getvalue(), '2\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
