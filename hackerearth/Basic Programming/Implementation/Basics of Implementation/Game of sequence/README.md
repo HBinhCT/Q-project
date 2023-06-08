73cd1647a914394542d54f14c37a56434fad480a
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2023-06-08 21:20:56 +0700

    [ADD] Game of sequence

---
+++ hackerearth/Basic Programming/Implementation/Basics of Implementation/Game of sequence/README.md
@@ -0,0 +1,1 @@
+https://www.hackerearth.com/practice/basic-programming/implementation/basics-of-implementation/practice-problems/algorithm/bracket-balancer-b9f56e12/
---
+++ hackerearth/Basic Programming/Implementation/Basics of Implementation/Game of sequence/test.py
@@ -0,0 +1,21 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+from unittest.mock import patch
+
+
+class TestQ(unittest.TestCase):
+    @patch('builtins.input', side_effect=[
+        '1',
+        '2',
+        '1 2',
+    ])
+    def test_case_0(self, input_mock=None):
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            import solution
+        self.assertEqual(text_trap.getvalue(), 'Q\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
