e360ebd109abfd91fac466fa96a7fbf8f1059b8f
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2023-06-17 19:37:18 +0700

    [ADD] Array Sum

---
+++ hackerearth/Basic Programming/Implementation/Basics of Implementation/Array Sum/README.md
@@ -0,0 +1,1 @@
+https://www.hackerearth.com/practice/basic-programming/implementation/basics-of-implementation/practice-problems/algorithm/array-sum-2-725368ac/
---
+++ hackerearth/Basic Programming/Implementation/Basics of Implementation/Array Sum/test.py
@@ -0,0 +1,20 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+from unittest.mock import patch
+
+
+class TestQ(unittest.TestCase):
+    @patch('builtins.input', side_effect=[
+        '5',
+        '1000000001 1000000002 1000000003 1000000004 1000000005',
+    ])
+    def test_case_0(self, input_mock=None):
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            import solution
+        self.assertEqual(text_trap.getvalue(), '5000000015\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
