f089c10353c4c53522372cd4a4f81dc585979d6f
QUANG-HUY-BINH PHAM <hbinhct@gmail.com>
2023-11-23 22:59:19 +0700

    [ADD] Double Beauty

---
+++ hackerearth/Algorithms/Greedy Algorithms/Basics of Greedy Algorithms/Double Beauty/README.md
@@ -0,0 +1,1 @@
+https://www.hackerearth.com/practice/algorithms/greedy/basics-of-greedy-algorithms/practice-problems/algorithm/double-beauty-711a7ca1/
---
+++ hackerearth/Algorithms/Greedy Algorithms/Basics of Greedy Algorithms/Double Beauty/test.py
@@ -0,0 +1,26 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+from unittest.mock import patch
+
+
+class TestQ(unittest.TestCase):
+    @patch(
+        "builtins.input",
+        side_effect=[
+            "2",
+            "5 3",
+            "1 4 1 3 1",
+            "3 1",
+            "2 5 1",
+        ],
+    )
+    def test_case_0(self, input_mock=None):
+        text_trap = io.StringIO()
+        with redirect_stdout(text_trap):
+            import solution
+        self.assertEqual(text_trap.getvalue(), "16\n" + "10\n")
+
+
+if __name__ == "__main__":
+    unittest.main()
